018eb7db2d5c652d11a483f51e8228216af1941a
WMXPY <wm@mengw.io>
2021-07-05 16:22:39 -0500

    add generate twofa url function

--- src/util.ts
+++ src/util.ts
@@ -8,13 +8,26 @@ import * as SpeakEasy from "speakeasy";
 
 export const generateTwoFAKey = (): string => {
 
-    const secret: SpeakEasy.GeneratedSecret = SpeakEasy.generateSecret({ length: 32 });
+    const secret: SpeakEasy.GeneratedSecret = SpeakEasy.generateSecret({
+
+        length: 32,
+    });
+
     return secret.base32;
+};
+
+export const generateTwoFAUrl = (issuer: string, account: string, key: string): string => {
+
+    const parsedIssuer: string = encodeURIComponent(issuer);
+    const parsedAccount: string = encodeURIComponent(account);
+
+    return 'otpauth://totp/' + parsedAccount + '?issuer=' + parsedIssuer + '&secret=' + key;
 };
 
 export const generateTwoFACode = (key: string): string => {
 
     return SpeakEasy.totp({
+
         secret: key,
         encoding: 'base32',
     });
@@ -23,8 +36,9 @@ export const generateTwoFACode = (key: string): string => {
 export const verifyTwoFACode = (key: string, code: string): boolean => {
 
     return SpeakEasy.totp.verify({
+
+        token: code,
         secret: key,
         encoding: 'base32',
-        token: code,
     });
 };
